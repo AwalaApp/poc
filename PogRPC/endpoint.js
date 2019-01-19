8d526f3ed1740494118f6531c90b060e1262fc10
Gustavo Narea <me@gustavonarea.net>
2019-01-19 19:12:11 +0000

    Complete implementation of Parcel delivery over gRPC (PogRPC)

---
+++ PogRPC/endpoint.js
@@ -0,0 +1,97 @@
+'use strict';
+// This is a proof of concept. The code below is ugly, inefficient and has no tests.
+
+// PogRPC Endpoint for Node.js.
+
+const grpc = require('grpc');
+const grpcProtoLoader = require('@grpc/proto-loader');
+const {createHash} = require('crypto');
+
+const pogrpcPackageDefinition = grpcProtoLoader.loadSync(
+    __dirname + '/pogrpc.proto',
+    {keepCase: true},
+);
+const pogrpcPackage = grpc.loadPackageDefinition(pogrpcPackageDefinition).relaynet.pogrpc;
+
+function makeClient() {
+    return new pogrpcPackage.PogRPC('localhost:21473', grpc.credentials.createInsecure());
+}
+
+function deliverParcels(parcels, client) {
+    // The final implementation should actually work with streams instead of loading everything in
+    // memory and doing so much stuff synchronously.
+
+    const sentParcelIds = new Set();
+    let allParcelsSent = false;
+
+    return new Promise((resolve, reject) => {
+        const call = client.deliverParcels();
+
+        call.on('data', function (deliveryAck) {
+            if (!sentParcelIds.has(deliveryAck.id)) {
+                call.end();
+                reject(new Error(`Got ACK for unknown parcel (${deliveryAck.id})`));
+                return;
+            }
+
+            sentParcelIds.delete(deliveryAck.id);
+
+            if (sentParcelIds.size === 0 && allParcelsSent) {
+                call.end();
+                resolve();
+            }
+        });
+
+        call.on('end', function () {
+            call.end();
+        });
+
+        for (let parcel of parcels) {
+            const parcelId = createHash('sha1').update(parcel).digest('hex');
+            call.write({id: parcelId, parcel});
+            sentParcelIds.add(parcelId);
+        }
+        allParcelsSent = true;
+
+        setTimeout(() => {
+            call.end();
+            reject(new Error('Timed out'));
+            // This should be propagated to the app in the final implementation so
+            // it can queue a retry for the unacknowledged parcels if necessary
+        }, 2000);
+    });
+}
+
+function collectParcels(client) {
+    // The final implementation should actually work with streams instead of loading everything in
+    // memory and doing so much stuff synchronously.
+    const receivedParcels = [];
+
+    return new Promise(function (resolve, reject) {
+        const call = client.collectParcels();
+
+        call.on('data', function (parcelDelivery) {
+            receivedParcels.push(parcelDelivery.parcel);
+
+            // The final implementation probably shouldn't send the ACK until the app
+            // has received and processed the parcel.
+            call.write({id: parcelDelivery.id});
+        });
+
+        call.on('end', function () {
+            call.end();
+            resolve(receivedParcels);
+        });
+
+        call.on('error', function (error) {
+            reject(error);
+            call.end();
+        });
+    });
+}
+
+module.exports = {
+    makeClient,
+    deliverParcels,
+    collectParcels,
+};
--- PogRPC/gateway.js
+++ PogRPC/gateway.js
@@ -10,25 +10,64 @@ const packageDefinition = protoLoader.loadSync(
 );
 const pogrpcPackage = grpc.loadPackageDefinition(packageDefinition).relaynet.pogrpc;
 
-function deliverParcel(call, callback) {
-    // TODO: Actually implement
-    console.log('Got parcel ', call.request.id);
-    callback(null, {});
+// Yup. This is the "database". And it's not even synchronized on disk. (It's a PoC!)
+const PARCELS_DB = {};
+
+function deliverParcels(call) {
+    call.on('data', function (parcelDelivery) {
+        PARCELS_DB[parcelDelivery.id] = parcelDelivery.parcel;
+        call.write({id: parcelDelivery.id}); // ACK
+    });
+
+    call.on('end', function () {
+        call.end();
+    });
 }
 
 function collectParcels(call) {
-    // TODO: Actually implement
-    const parcels = [];
-    for (let parcel of parcels) {
-        call.write({id: 42, parcel: Buffer.from('Answer to all questions')});
+    if (Object.keys(PARCELS_DB).length === 0) {
+        call.end();
+        return;
     }
-    call.end();
+
+    const pendingAckParcelIds = new Set();
+
+    call.on('data', function (deliveryAck) {
+        if (pendingAckParcelIds.has(deliveryAck.id)) {
+            delete PARCELS_DB[deliveryAck.id];
+            pendingAckParcelIds.delete(deliveryAck.id);
+        }
+    });
+
+    call.on('end', function () {
+        call.end();
+    });
+
+    for (const [parcelId, parcel] of Object.entries(PARCELS_DB)) {
+        call.write({id: parcelId, parcel});
+        pendingAckParcelIds.add(parcelId);
+    }
+
+    call.on('data', function () {
+        if (pendingAckParcelIds.size === 0) {
+            call.end();
+        }
+    });
+
+    setTimeout(() => {
+        if (0 < pendingAckParcelIds.size) {
+            call.end();
+            console.error('Endpoint took too long to acknowledge all parcel collections');
+            // This should be propagated to the app in the final implementation so
+            // it can queue a retry for the unacknowledged parcels if necessary
+        }
+    }, 2000);
 }
 
 function runServer(ip = '127.0.0.1', port = 21473) { // 21473? Too late!
     const server = new grpc.Server();
     server.addService(pogrpcPackage.PogRPC.service, {
-        deliverParcel,
+        deliverParcels,
         collectParcels,
     });
     server.bind(`${ip}:${port}`, grpc.ServerCredentials.createInsecure());
